95ff182f1966facd6e2d62a8fa9a9f098f23f0a6
ClickyKeys <lkn56999@gmail.com>
2022-08-09 10:51:01 -0500

    added link to CV on all pages

--- site/js/linkopen.js
+++ site/js/linkopen.js
@@ -1,17 +1,1 @@
-$(document).ready(function () {
-    setTimeout(function () {
-
-          $('a[href]').each(function () {
-              var href = this.href;
-
-              $(this).removeAttr('href').css('cursor', 'pointer').click(function () {
-                  if (href.toLowerCase().indexOf("#") >= 0) {
-
-                  } else {
-                      window.open(href, '_blank');
-                  }
-              });
-          });
-
-    }, 500);
-});
+$('a[href]').remove(); 
